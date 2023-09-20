c04d9e202b97baca666a585ac12c44ec8fe238c0
cashhahuynh <cashhahuynh@gmail.com>
2023-09-19 22:50:12 -0400

    9/19 ConflictsSubmitter method ideas + json file

--- src/main/java/StaticMethods/ConflictsSubmitter.java
+++ src/main/java/StaticMethods/ConflictsSubmitter.java
@@ -1,13 +1,39 @@
 package StaticMethods;
 
 import DataManagement.LinkedListConflicts;
+import DataManagement.LinkedListNode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConflictsSubmitter {
 
-    //this method will handle returning conflicts results
+    private static int hits;
+    private static List<String> toSearchList = new ArrayList<>();
+    private static List<String> conflictsReportList = new ArrayList<>();
+
+    //create list from linkedlist object
     public static void runConflicts(LinkedListConflicts list) {
         System.out.println("\nThanks for submitting. We will run a search and generate a report shortly for the parties below.");
         LinkedListConflicts.display(list.head);
+
+        //LinkedListConflicts object calls display method, which takes LinkedListNode as a parameter
+        
+    }
+
+    //purpose is to search in database before adding new search
+    public static void searchDataBase(List<String> list) {
+        toSearchList = list;
+    }
+
+    //purpose is to add to existing database- create json object and add to json file
+    public static void addToDataBase() {
+
+    }
+
+    //will be responsible for returning conflict report
+    public static void generateReport() {
+
     }
 
 }
